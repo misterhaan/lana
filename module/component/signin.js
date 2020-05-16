2f8c9ad1aa4b7e9ea890760d08db1996a1905116
misterhaan <misterhaan@track7.org>
2020-05-15 22:51:45 -0500

    turn close popup directive into a mixin

--- module/component/signin.js
+++ module/component/signin.js
@@ -1,3 +1,5 @@
+import ClosePopup from "../mixin/closePopup.js";
+
 export default {
 	data() {
 		return {
@@ -9,39 +11,9 @@ export default {
 			this.showMenu = false;
 		}
 	},
-	directives: {
-		"close-popup": {
-			bind(el, binding) {
-				if(!document.popupStack) {
-					document.popupStack = [];
-					$(document).keyup(e => {
-						if(e.key == "Escape")
-							for(let popup = document.popupStack.pop(); popup; popup = document.popupStack.pop())
-								if(document.body.contains(popup)) {
-									popup.popupHide(e);
-									return;  // leave other popups on the stack
-								}
-					}).click(e => {
-						for(let popup = document.popupStack.pop(); popup; popup = document.popupStack.pop())
-							if(document.body.contains(popup)) {
-								if(popup == e.target || popup.contains(e.target))
-									document.popupStack.push(popup);  // clicked the popup, so keep it on the stack and don't hide it
-								else
-									popup.popupHide(e);
-								return;  // leave the other popups on the stack
-							}
-					});
-				}
-				el.popupHide = binding.value;
-				document.popupStack.push(el);
-			},
-			unbind(el) {
-				const index = document.popupStack.indexOf(el);
-				if(index > -1)
-					document.popupStack.splice(index, 1);
-			}
-		}
-	},
+	mixins: [
+		ClosePopup
+	],
 	template: /*html*/ `
 		<div id=userstatus>
 			<a id=usertrigger @click.prevent.stop="showMenu = !showMenu" :class="{open: showMenu}">Sign In</a>
