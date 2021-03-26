34c8c6c1c35d9cd59eb7c0fac365bd22101c50d7
misterhaan <misterhaan@track7.org>
2021-03-25 22:30:41 -0500

    tab stop for go home

--- module/component/titlebar.js
+++ module/component/titlebar.js
@@ -2,29 +2,23 @@ import AppName from "../appName.js";
 import Signin from "./signin.js";
 import PlayerMenu from "./playerMenu.js";
 
-export default {
+const TitleBar = {
 	props: [
 		"player",
 		"auths"
 	],
-	methods: {
-		GoHome() {
-			if(location.hash)
-				location.hash = "";
-			if(!location.pathname.endsWith("/"))
-				location.pathname = location.pathname.substr(0, location.pathname.lastIndexOf("/"));
-		}
-	},
 	components: {
 		signin: Signin,
 		playerMenu: PlayerMenu
 	},
 	template: /*html*/ `
-		<header id=titlebar>
-			<img id=favicon src=favicon.svg alt="" @click=GoHome>
-			<h2 id=sitetitle @click=GoHome>${AppName.Full}</h2>
+		<header id=titleBar>
+			<a href=. id=goHome>
+				<img id=favicon src=favicon.svg alt=""><span id=siteTitle>${AppName.Full}</span>
+			</a>
 			<signin v-if="!player && auths" :auths=auths></signin>
 			<playerMenu v-if=player :player=player></playerMenu>
 		</header>
 	`
-}
+};
+export default TitleBar;
