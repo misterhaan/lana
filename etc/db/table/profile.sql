2dd354a842a55c10fb58f25a74e0bda25e3098a9
misterhaan <misterhaan@track7.org>
2026-02-04 13:31:16 -0600

    require profile visibility

--- etc/db/table/profile.sql
+++ etc/db/table/profile.sql
@@ -3,5 +3,5 @@ create table profile (
 	name varchar(100) comment 'Display name of this profile',
 	url varchar(200) comment 'URL to this profile on the external site',
 	avatar varchar(200) comment 'URL to the avatar for this profile',
-	visibility tinyint(1) unsigned comment 'Who can see this profile'
+	visibility tinyint(1) unsigned not null comment 'Who can see this profile'
 ) comment 'Players can use an avatar from their profiles on other websites';
